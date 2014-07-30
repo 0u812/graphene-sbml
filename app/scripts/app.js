28eae5a63b6a993ff1e90d276d87bbb3de4d064a
Stanley Gu <stanleygu@gmail.com>
2014-07-30 13:34:36 -0700

    Added angular-strapdependency

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -9,4 +9,4 @@
  * Main module of the application.
  */
 angular
-  .module('sg.graphene.sbml', ['sg.graphene', 'ui.bootstrap']);
+  .module('sg.graphene.sbml', ['sg.graphene', 'ui.bootstrap', 'mgcrea.ngStrap.aside', 'mgcrea.ngStrap.modal']);
